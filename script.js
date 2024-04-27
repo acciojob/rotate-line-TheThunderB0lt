976d944adb571b58ff5fdc79dfb0a0aa967ed3d5
NIKITH KUMAR N <nikithkumarniki160698@gmail.com>
2024-04-27 23:07:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const line = document.getElementById('line');
+
+  // Initialize angle
+  let angle = 0;
+
+  // Function to update rotation
+  function rotateLine() {
+    angle += 2; // Increase angle by 2 degrees
+    line.style.transform = ` rotate(${angle}deg)`;
+    // 
+	  setTimeout(rotateLine, 20);
+  }
+
+  // Start rotating the line
+  rotateLine();
